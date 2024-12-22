6f18cad6c2961fc6a52f575b9bdf66a653f1ef17
Nicolas Martyanoff <nicolas@n16f.net>
2024-12-22 18:48:43 +0100

    add functions to access blocks

--- bcl.go
+++ bcl.go
@@ -1,6 +1,8 @@
 package bcl
 
-import "io"
+import (
+	"io"
+)
 
 type Document struct {
 	Source   string
@@ -38,4 +40,38 @@ func Parse(data []byte, source string) (*Document, error) {
 func (doc *Document) Print(w io.Writer) error {
 	p := newPrinter(w, doc)
 	return p.Print()
+}
+
+func (doc *Document) Blocks(name string) []*Block {
+	return doc.TopLevel.Blocks(name)
+}
+
+func (doc *Document) Block(name, label string) *Block {
+	return doc.TopLevel.Block(name, label)
+}
+
+func (b *Block) Blocks(name string) []*Block {
+	var blocks []*Block
+
+	for _, elt := range b.Elements {
+		if block, ok := elt.Content.(*Block); ok {
+			if block.Name == name {
+				blocks = append(blocks, block)
+			}
+		}
+	}
+
+	return blocks
+}
+
+func (b *Block) Block(name, label string) *Block {
+	for _, elt := range b.Elements {
+		if block, ok := elt.Content.(*Block); ok {
+			if block.Name == name && block.Label == label {
+				return block
+			}
+		}
+	}
+
+	return nil
 }
